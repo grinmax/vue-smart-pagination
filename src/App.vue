c8ecd82c91766eba0a937fc4bbd1c60f2532b01e
grinmax <maksim.hrynevich@qweeco.com>
2018-02-13 13:17:40 +0300

    #first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="copyright">&copy; Copyright 2018 Private enterprise "Web Friends"<br /><a href="https://www.webfriends.by">www.webfriends.by</a></div>
+    <Pagination/>
+  </div>
+</template>
+
+<script>
+import Pagination from './components/Pagination'
+
+export default {
+  name: 'App',
+  components: {
+    Pagination
+  }
+}
+</script>
+
+<style lang="scss">
+  .copyright {
+    padding: 30px 0 20px 0;
+    text-align: center;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-size: 18px;
+    opacity: 0.6;
+
+    a {
+      color: #02C8F3;
+
+      &:visited {
+        color: #02C8F3;
+      }
+    }
+  }
+</style>
